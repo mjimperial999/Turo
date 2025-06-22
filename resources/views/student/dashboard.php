8a6ba2af3aa08a0b35d0548a090310f13ecc631a
mjimperial999 <202210383@feudiliman.edu.ph>
2025-06-22 16:34:03 +0800

    Ready

--- resources/views/student/dashboard.php
+++ resources/views/student/dashboard.php
@@ -1,14 +1,5 @@
-<?php /*
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | Turo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/css/styles.css">
-    */ ?>
+<?php $title = 'My Courses';
+include __DIR__ . '/../partials/head.php';?>
     <style>
         h4 {
             font-family: Alata, sans-serif;
@@ -101,9 +92,7 @@
 </head>
 
 <body>
-    <?php $title = 'My Courses';
-include __DIR__ . '/../partials/head.php';
-include __DIR__ . '/../partials/nav.php'; ?> 
+    <?php include __DIR__ . '/../partials/nav.php'; ?> 
 
     <div class="home-tutor-screen">
         <div class="home-tutor-main">
